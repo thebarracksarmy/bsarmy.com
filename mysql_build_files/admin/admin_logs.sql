f8d47f2a13e004d3afd71e3c508c978363176703
Lucas Burlingham <lucas@lucasburlingham.me>
2023-09-14 19:19:21 -0400

    Add ip to admin_logs table

--- mysql_build_files/admin/admin_logs.sql
+++ mysql_build_files/admin/admin_logs.sql
@@ -2,7 +2,9 @@ CREATE TABLE `admin_logs` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`epoch_time` INT NOT NULL,
 	`admin_username` TINYTEXT,
+	`ip` TINYTEXT NOT NULL,
 	`description` TEXT NOT NULL,
 	`category` TINYTEXT,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
+
