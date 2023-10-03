0f761227922f2ed8d75c5fb7ae194c97d2d5f672
Lucas Burlingham <lucas@lucasburlingham.me>
2023-10-03 01:34:43 +0000

    Update color scheme

--- error/404/index.php
+++ error/404/index.php
@@ -23,7 +23,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/beforeIncludes.php';
 		<div class="container-fluid py-5">
 			<h1 class="display-5 fw-bold"><kbd>http error 404</kbd> Page or resource not found.</h1>
 			<p class="fst-italics">Sorry, the page you are looking for does not exist.</p>
-			<a href="/index.php" class="btn btn-primary">Return to Home</a>
+			<a href="/index.php" class="btn btn-light-tan">Return to Home</a>
 			<!-- go back to page referrer -->
 			<a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" class="btn btn-secondary">Go back</a>
 		</div>
