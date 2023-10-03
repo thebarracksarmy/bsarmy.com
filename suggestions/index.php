683d44a922fffc004b3ba5e46ec848274807b6bc
Lucas Burlingham <lucas@lucasburlingham.me>
2023-10-03 01:33:05 +0000

    Add /suggestions/

---
+++ suggestions/index.php
@@ -0,0 +1,7 @@
+<?php
+
+header('Location: https://forms.gle/F9c7CEvEkwV6TbZNA');
+
+echo "Redirecting to <a href='https://forms.gle/F9c7CEvEkwV6TbZNA'>https://forms.gle/F9c7CEvEkwV6TbZNA</a>"
+
+?>
