08501963e8a3b2838d809338f99f2e4bc66ecdee
Lucas Burlingham <lucas@lucasburlingham.me>
2023-10-04 20:16:18 +0000

    Add new footer

--- assets/includes/elements/footer.php
+++ assets/includes/elements/footer.php
@@ -2,54 +2,63 @@
 
 
 ?>
-	<footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-    <div class="col mb-3">
-      <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-      <p class="text-body-secondary">© Lucas Burlingham 2023</p>
-      <p class="text-body-secondary">bsarmy.com is not affiliated with the DoD.</p>
 
-    </div>
+<div class="container">
+<footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+	<div class="col mb-3">
+		<a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+			<!-- bsarmy.com logo -->
+			<img src="/assets/icons/logo.png" height="80px" width="215px" class="rounded"/>
+		</a>
+		<p class="text-body-secondary">© Lucas Burlingham 2023</p>
+		<p class="text-body-secondary">bsarmy.com is not affiliated with the DoD.</p>
 
-    <div class="col mb-3">
+	</div>
 
-    </div>
+	<div class="col mb-3">
 
-    <div class="col mb-3">
-      <h5>Section</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="https://bsarmy.com/" class="nav-link p-0 text-body-secondary">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+	</div>
+
+	<div class="col mb-3">
+		<h5>Navigation</h5>
+		<ul class="nav flex-column">
+			<li class="nav-item mb-2"><a href="https://bsarmy.com/" class="nav-link p-0 text-body-secondary">Home</a></li>
+			<li class="nav-item mb-2"><a href="https://bsarmy.com/login/" class="nav-link p-0 text-body-secondary">Login</a></li>
+			<li class="nav-item mb-2"><a href="https://bsarmy.com/register/" class="nav-link p-0 text-body-secondary">Signup</a></li>
+			<li class="nav-item mb-2"><a href="https://bsarmy.com/contact/" class="nav-link p-0 text-body-secondary disabled" disabled>Contact</a></li>
+			<li class="nav-item mb-2"><a href="https://bsarmy.com/about/" class="nav-link p-0 text-body-secondary disabled" disabled>About</a></li>
+		</ul>
+	</div>
+
+	<div class="col mb-3">
+		<h5>DFAC</h5>
+		<ul class="nav flex-column">
+			<li class="nav-item mb-2">
+				<a href="https://bsarmy.com/DFAC/" class="nav-link p-0 text-body-secondary" rel="noopener noreferrer">
+					<?php echo date('F Y'); ?> Schedule
+				</a>
+			</li>
+	<!-- <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
         <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
         <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
         <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-      </ul>
-    </div>
+	 -->
+		</ul>
+	</div>
 
-    <div class="col mb-3">
-      <h5>DFAC</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2">
-			<a href="#" class="nav-link p-0 text-body-secondary">
-			<?php echo date('F Y'); ?> Schedule
-			</a>
-		</li>
-        <!-- <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li> -->
-      </ul>
-    </div>
-
-    <div class="col mb-3">
-      <h5>Help & Terms</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Submit an Issue</a></li>
-        <li class="nav-item mb-2"><a href="https://github.com/lucasburlingham/bsarmy.com/" class="nav-link p-0 text-body-secondary">Start Contributing on <i class="fa fa-github" aria-hidden="true"></i></a></li>
-        <li class="nav-item mb-2"><a href="https://bsarmy.com/docs/terms_conditions/" class="nav-link p-0 text-body-secondary">Terms & Conditions</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary disabled">About</a></li>
-      </ul>
-    </div>
-  </footer>
+	<div class="col mb-3">
+		<h5>Help & Terms</h5>
+		<ul class="nav flex-column">
+			<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Submit an Issue</a></li>
+			<li class="nav-item mb-2">
+				<a href="https://github.com/lucasburlingham/bsarmy.com/" class="nav-link p-0 text-body-secondary">
+					Start Contributing on <i class="fa-brands fa-github" alt="GitHub"></i>
+				</a>
+			</li>
+			<li class="nav-item mb-2"><a href="https://bsarmy.com/docs/terms_conditions/" class="nav-link p-0 text-body-secondary">Terms & Conditions</a></li>
+			<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary disabled">FAQs</a></li>
+			<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary disabled">About</a></li>
+		</ul>
+	</div>
+</footer>
+</div>
