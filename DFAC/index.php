624a667932d50d1564266032b8555e313d34bd6f
Lucas Burlingham <lucas@lucasburlingham.me>
2023-10-30 16:47:15 +0000

    Automate date changing

--- DFAC/index.php
+++ DFAC/index.php
@@ -23,10 +23,15 @@ DFAC event system (aka Taco Tuesday, Pizza Friday, etc.)
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/beforeIncludes.php';
 
+$full_month = strtolower(date('F'));
+$month = date('m');
+$year = date('Y');
+$base = "liberty";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -35,70 +40,89 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/beforeIncludes.php';
 	<!-- Use includes to insert snippets of code that will be reused in every page -->
 	<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/elements/head.php'; ?>
 </head>
+
 <body>
 	<!-- The navbar won't change so insert it for a more consistant exprience -->
 	<!-- TODO: figure out how to pass the active page to make it aria accessable -->
 	<?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/elements/nav.php'; ?>
 
 	<section class="py-5 text-center container">
-			<div class="row py-lg-3">
-				<h3 class="h3">
-					<?php echo date('F Y'); ?> &middot; <span class="text-muted" disabled>
+		<div class="row py-lg-3">
+			<h3 class="h3">
+				<?php echo date('F Y'); ?> &middot; <span class="text-muted" disabled>
 					Fort Liberty, NC
-					</span>
-				</h3>
-				<!-- Show JPG version of schedule for the current month -->
-				<!-- Image name syntax is as follows: -->
-				<!-- /DFAC/schedules/YYYY/MMM/liberty_mmmmm_yyyy.jpg -->
-				<!-- /DFAC/schedules/YYYY/MMM/liberty_mmmmm_yyyy.pdf -->
-				<!-- /DFAC/schedules/YYYY/MMM/liberty_mmmmm_yyyy.png -->
+				</span>
+			</h3>
+			<!-- Show JPG version of schedule for the current month -->
+			<!-- Image name syntax is as follows: -->
+			<!-- /DFAC/schedules/YYYY/MMM/liberty_mmmmm_yyyy.jpg -->
+			<!-- /DFAC/schedules/YYYY/MMM/liberty_mmmmm_yyyy.pdf -->
+			<!-- /DFAC/schedules/YYYY/MMM/liberty_mmmmm_yyyy.png -->
 
-				<div class="container">
-					<div class="row">
-						<div class="col">
-							<a href="/DFAC/schedules/2023/OCT/liberty_october_2023.jpg" target="_blank" rel="noopener noreferrer">
-								<img src="/DFAC/schedules/2023/OCT/liberty_october_2023.jpg" alt="Fort Liberty, NC DFAC Schedule for October 2023" class="img-fluid" />
-							</a>
-							<!-- Show links to download different versions -->
-							<br>
-							<hr style="width: 50%;" class="mx-auto my-1">
-							<div class="text-muted mt-1">
-								Download a different format:
-								<?php 
-$formats = ['pdf', 'png', 'jpg'];
-$full_month = strtolower(date('F'));
-$month = date('m');
-$year = date('Y');
+			<div class="container">
+				<div class="row">
+					<div class="col">
 
-foreach ($formats as $format) {
+						<?php
+							$filename = $base . '_' . $full_month . '_' . $year . '.jpg';
 
-	$format_upper = strtoupper($format);
-	$base = "liberty";
-	$filename = $base . '_' . $full_month . '_' . $year . '.' . $format;
+							// echo '/DFAC/schedules/' . $year . '/' . $month . '/' . $filename;
 
-$href = <<<EOT
-								<a href="/DFAC/schedules/$year/$month/$filename" target="_blank" rel="noopener noreferrer" class="text-muted">$format_upper version</a>&middot;
-EOT;
-									echo $href;
-}
+							if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/DFAC/schedules/' . $year . '/' . $month . '/' . $filename)) {
+
+								$schedule = <<<EOT
+								<a href='/DFAC/schedules/' . $year . '/' . $month . '/' . $filename' target="_blank" rel="noopener noreferrer">
+									<img src='/DFAC/schedules/$year/$month/$filename' alt="Fort Liberty, NC DFAC Schedule for date('F Y') " class="img-fluid" />
+								</a>
+								<!-- Show links to download different versions -->
+								<br>
+								<hr style="width: 50%;" class="mx-auto my-1">
+								<div class="text-muted mt-1">
+									Download a different format: 
+								EOT;
+
+								echo $schedule;
+
+									// iterate through each format and output a link to it
+									$formats = ['pdf', 'png', 'jpg'];
+
+									foreach ($formats as $format) {
+										// Prepare filename
+										$format_upper = strtoupper($format);
+										$base = "liberty";
+										$filename = $base . '_' . $full_month . '_' . $year . '.' . $format;
+
+										// Construct link
+										$href = <<<EOT
+										<a href="/DFAC/schedules/$year/$month/$filename" target="_blank" rel="noopener noreferrer" class="text-muted">$format_upper version</a>&middot;
+										EOT;
+
+										// Output link to page
+										echo $href;
+									}
+
+							} else {
+								echo '<p class="text-muted">Schedule for '. date('F Y') .' not found. Please check back later.<p>';
+								echo '<p class="text-muted">If you believe this is an error, please email <a href="mailto:support@bsarmy.com">support@bsarmy.com</a>.</p>';
+							}
+							
 								?>
-							</div>
-						</div>
 					</div>
 				</div>
 			</div>
-		</section>
-		<section class="container text-center py-5">
-			<div class="row">
-				<div class="col">
-					<h3>
-						Archived Schedules:
-					</h3>
-					<p class="text-muted">
-						None at the moment. Check back next month!
+		</div>
+	</section>
+	<section class="container text-center py-5">
+		<div class="row">
+			<div class="col">
+				<h3>
+					Archived Schedules:
+				</h3>
+				<p class="text-muted">
+					None at the moment. Check back next month!
 
 
-						<?php
+					<?php
 
 $year = date('Y');
 
@@ -117,16 +141,16 @@ asort($monthsavaliable, SORT_NUMERIC);
 
 $monthsbefore = array_pop($monthsavaliable); // remove the current (last) month from the array
 
-print_r($yearsavaliable);
-print_r($monthsbefore);
+// print_r($yearsavaliable);
+// print_r($monthsbefore);
 
 						?>
-					</p>
-				</div>
+				</p>
 			</div>
-		</section>
+		</div>
+	</section>
 
-		<?php
+	<?php
 			// Add footer to page
 			require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/elements/footer.php';
 		?>
