31d78c3bbf84f2828ff98cbe77bf6d6372bf9a63
Lucas Burlingham <lucas@lucasburlingham.me>
2023-11-02 00:54:29 +0000

    Don't use log_server_command (it's not ready yet)

--- admin/index.php
+++ admin/index.php
@@ -6,23 +6,23 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/beforeIncludes.php';
 
 
 
-if(isset($_POST['git_pull']) && $_POST['git_pull'] == 'git_pull') {
-	shell_exec('git pull');
-	// Need to use HTTP_CF_CONNECTING_IP because Cloudflare Tunnels works behind a proxy to serve the site
-	// https://stackoverflow.com/questions/14985518/cloudflare-and-logging-visitor-ip-addresses-via-in-php
-	shell_exec('wall "Git Pull Complete at ' . date('Y-m-d H:i:s') . ' by ' . $_SERVER['HTTP_CF_CONNECTING_IP'] . ' as ' . $_SERVER['PHP_AUTH_USER'] . '"');
+// if(isset($_POST['git_pull']) && $_POST['git_pull'] == 'git_pull') {
+// 	shell_exec('git pull');
+// 	// Need to use HTTP_CF_CONNECTING_IP because Cloudflare Tunnels works behind a proxy to serve the site
+// 	// https://stackoverflow.com/questions/14985518/cloudflare-and-logging-visitor-ip-addresses-via-in-php
+// 	shell_exec('wall "Git Pull Complete at ' . date('Y-m-d H:i:s') . ' by ' . $_SERVER['HTTP_CF_CONNECTING_IP'] . ' as ' . $_SERVER['PHP_AUTH_USER'] . '"');
 
-	// add action to mysql database admin table
-	$result = log_server_command($_SERVER['PHP_AUTH_USER'], $_SERVER['HTTP_CF_CONNECTING_IP'], 'git_pull', 1);
+// 	// add action to mysql database admin table
+// 	$result = log_server_command($_SERVER['PHP_AUTH_USER'], $_SERVER['HTTP_CF_CONNECTING_IP'], 'git_pull', 1);
 
-	echo $result;
+// 	echo $result;
 
-} else {
-	// add action to mysql database admin table
-	$result = log_server_command($_SERVER['PHP_AUTH_USER'], $_SERVER['HTTP_CF_CONNECTING_IP'], 'git_pull', 0);
+// } else {
+// 	// add action to mysql database admin table
+// 	$result = log_server_command($_SERVER['PHP_AUTH_USER'], $_SERVER['HTTP_CF_CONNECTING_IP'], 'git_pull', 0);
 
-	echo $result;
-}
+// 	echo $result;
+// }
 
 ?>
 
@@ -32,10 +32,10 @@ if(isset($_POST['git_pull']) && $_POST['git_pull'] == 'git_pull') {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>The Barracks Admin - Git Pull</title>
+	<title>The Barracks Admin - Main</title>
 	<?php
 
-include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/headIncludes.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/elements/head.php';
 
 ?>
 
@@ -50,6 +50,8 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/includes/headIncludes.php';
 		<input type="text" value="git_pull" name="git_pull" hidden>
 		<input type="submit" value="Git Pull">
 	</form>
+
+	<a href="manageusers.php" target="_blank" rel="noopener noreferrer">Manage Users</a>
 </body>
 
 </html>
