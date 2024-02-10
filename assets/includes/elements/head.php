9b24832b9681db264d9619420d276cd56f105205
Lucas Burlingham <lucas@lucasburlingham.me>
2024-02-10 17:43:37 +0000

    Revert to specific versions of bootstrap, popper, etc

--- assets/includes/elements/head.php
+++ assets/includes/elements/head.php
@@ -49,21 +49,21 @@ echo <<<EOT
 	<!-- <link rel="manifest" href="/manifest.json"> -->
 
 	<!-- Bootstrap (https://getbootstrap.com) -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" 
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
 		integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
-		integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-	</script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" 
-		integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
-	</script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+		integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
+		crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+		integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+		crossorigin="anonymous"></script>
 
 		
 	<!-- htmx (https://htmx.org/)-->
 	<script src="https://unpkg.com/htmx.org@1.9.5"></script>
 
 	<!-- less.js (http://lesscss.org/) -->
-	<script src="https://cdn.jsdelivr.net/npm/less" ></script>
+	<script src="https://cdn.jsdelivr.net/npm/less"></script>
 
 	<!-- Project CSS -->
 	<link rel="stylesheet" href="/assets/css/fonts.css?$cacheVersion">
@@ -109,3 +109,4 @@ echo <<<EOT
 		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 EOT;
+
